5c68af68ed8e31fa6b166537c933bcdc4b07806d
Padmanabha Roy <73742277+roypadmanabha@users.noreply.github.com>
2023-04-18 16:15:29 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6a045032cbmsh21ff342a703c37cp1236bbjsn1a54d56343a5",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "018d9a2cb3664a65b135d108853a8895";
